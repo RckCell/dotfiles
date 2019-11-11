76a382f2b9d06a26601e36cb2b7003789d68f33b
Pablo Talavera <pabloant86@gmail.com>
2019-11-11 11:10:19 -0300

    new St config

--- Source/st/config.h
+++ Source/st/config.h
@@ -87,34 +87,34 @@ float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-          [0] = "#282a2e", /* black   */
-          [1] = "#a54242", /* red     */
-          [2] = "#8c9440", /* green   */
-          [3] = "#de935f", /* yellow  */
-          [4] = "#5f819d", /* blue    */
-          [5] = "#85678f", /* magenta */
-          [6] = "#5e8d87", /* cyan    */
-          [7] = "#707880", /* white   */
+ /* 8 normal colors */
+    [0] = "#282a2e", /* black   */
+    [1] = "#a54242", /* red     */
+    [2] = "#8c9440", /* green   */
+    [3] = "#de935f", /* yellow  */
+    [4] = "#5f819d", /* blue    */
+    [5] = "#85678f", /* magenta */
+    [6] = "#5e8d87", /* cyan    */
+    [7] = "#707880", /* white   */
 
-        /* 8 bright colors */
-          [8]  = "#373b41", /* black   */
-          [9]  = "#cc6666", /* red     */
-          [10] = "#b5bd68", /* green   */
-          [11] = "#f0c674", /* yellow  */
-          [12] = "#81a2be", /* blue    */
-          [13] = "#b294bb", /* magenta */
-          [14] = "#8abeb7", /* cyan    */
-          [15] = "#c5c8c6", /* white   */
+  /* 8 bright colors */
+    [8]  = "#373b41", /* black   */
+    [9]  = "#cc6666", /* red     */
+    [10] = "#b5bd68", /* green   */
+    [11] = "#f0c674", /* yellow  */
+    [12] = "#81a2be", /* blue    */
+    [13] = "#b294bb", /* magenta */
+    [14] = "#8abeb7", /* cyan    */
+    [15] = "#c5c8c6", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-        "#e4e4e4",
-        "#303030",
-        "#252525",
+    "#e4e4e4",
+    "#303030",
+    "#252525",
 };
 
 
@@ -157,13 +157,23 @@ static unsigned int mousebg = 0;
 static unsigned int defaultattr = 11;
 
 /*
+ * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
+ * Note that if you want to use ShiftMask with selmasks, set this to an other
+ * modifier, set to 0 to not use it.
+ */
+static uint forcemousemod = ShiftMask;
+
+/*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
 static MouseShortcut mshortcuts[] = {
-	/* button               mask            string */
-	{ Button4,              XK_ANY_MOD,     "\031" },
-	{ Button5,              XK_ANY_MOD,     "\005" },
+	/* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
+	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -203,10 +213,6 @@ static Shortcut shortcuts[] = {
  * * 0: no value
  * * > 0: cursor application mode enabled
  * * < 0: cursor application mode disabled
- * crlf value
- * * 0: no value
- * * > 0: crlf mode is enabled
- * * < 0: crlf mode is disabled
  *
  * Be careful with the order of the definitions because st searches in
  * this table sequentially, so any XK_ANY_MOD must be in the last
@@ -224,13 +230,6 @@ static KeySym mappedkeys[] = { -1 };
  * numlock (Mod2Mask) and keyboard layout (XK_SWITCH_MOD) are ignored.
  */
 static uint ignoremod = Mod2Mask|XK_SWITCH_MOD;
-
-/*
- * Override mouse-select while mask is active (when MODE_MOUSE is set).
- * Note that if you want to use ShiftMask with selmasks, set this to an other
- * modifier, set to 0 to not use it.
- */
-static uint forceselmod = ShiftMask;
 
 /*
  * This is the huge key array which defines all compatibility to the Linux
